f4f6349057272492afe0e92b3a7ade7d2e81e8c9
Jack Henhapl <jackah2@illinois.edu>
2024-03-15 00:56:26 -0500

    Add bulk of logic

--- src.py
+++ src.py
@@ -1,11 +1,12 @@
 import json
-
-from datetime import datetime, time
+import signal
+import sys
+from datetime import datetime, date, time
 from pathlib import Path
+from time import sleep
 
 import pandas as pd
 import sqlalchemy as db
-
 from websockets.sync.client import connect
 
 
@@ -13,8 +14,60 @@ with open(Path(__file__).parent / "credentials.json") as f:
     creds = json.load(f)
 ENG = db.create_engine(db.URL.create("mysql+mysqlconnector", **creds["database"]))
 
+TODAY = datetime.combine(date.today(), time.min)
 
-def send_trigger(username, trigger_name):
+print("Connecting to socket... ")
+SOCKET = connect(
+    "wss://free.blr2.piesocket.com/v3/qrfchannel?api_key=4TRTtRRXmvNwXCWUFIjgKLDdZJ0zwoKpzn5ydd7Y&notify_self=1"
+)
+print("Connected!")
+
+GET_ONLINE_PLAYERS = """
+select usr.user as online_user, last_time as login_time from (
+    select user
+         , max(time) as last_time
+    from co_session
+    where time >= unix_timestamp(current_date() - 1) -- only look back 1 day
+    group by user
+) as last_times
+left join co_session as sess on last_times.user = sess.user and last_times.last_time = sess.time
+left join co_user as usr on last_times.user = usr.rowid
+where sess.action = 1
+"""
+
+GET_COMMANDS = """
+select from_unixtime(c.time) as time
+     , u.user as username
+     , message
+     , world, x, y, z
+from (
+    select * from co_command where from_unixtime(time) >= '{newer_than}'
+) as c
+left join co_user as u on c.user = u.rowid
+left join co_world as w on c.wid = w.rowid
+"""
+
+GET_OBSERVATIONS = """
+select from_unixtime(floor(time / 1000)) as time
+     , username
+     , observation_color_stripped as observation
+     , world, x, y, z
+from whimc_observations
+where from_unixtime(time / 1000) >= '{newer_than}'
+"""
+
+GET_SCIENCE_TOOLS = """
+select from_unixtime(time / 1000) as time
+     , username
+     , tool
+     , measurement
+     , world, x, y, z
+from whimc_sciencetools
+where from_unixtime(time / 1000) >= '{newer_than}'
+"""
+
+
+def send_trigger(trigger_name, username):
     payload = {
         "event": "new_message",
         "data": {
@@ -24,6 +77,7 @@ def send_trigger(username, trigger_name):
             "eventID": "",
             "student": username,
             "trigger": trigger_name,
+            "priority": 1,
         },
     }
     # Have to populate 'masterlogs' with 'data' plus a few empty fields
@@ -34,45 +88,72 @@ def send_trigger(username, trigger_name):
         "feedbackTXT": "",
         "feedbackREC": "",
     }
-
-    # replace '1' with 'qrfchannel
-    with connect(
-        "wss://free.blr2.piesocket.com/v3/1?api_key=4TRTtRRXmvNwXCWUFIjgKLDdZJ0zwoKpzn5ydd7Y&notify_self=1"
-    ) as websocket:
-        websocket.send(json.dumps(payload))
-        message = websocket.recv()
-        print(f"Received: {message}")
+    SOCKET.send(json.dumps(payload))
+    SOCKET.recv()
 
 
-def get_commands(last_query: datetime | None = None) -> tuple[pd.DataFrame, datetime]:
-    all_users = """
-    select cmd.time as time
-        , usr.user as username
-        , message
-    from co_command as cmd
-    left join co_user as usr
-    on cmd.user = usr.rowid;
-    """
-    return pd.read_sql(all_users, ENG), datetime.now()
+def get_data(query, newer_than: datetime = TODAY) -> pd.DataFrame:
+    return pd.read_sql(query.format(newer_than=newer_than), ENG)
 
 
-def get_online_players():
-    online_users = """
-        select usr.user as online_user, last_time as login_time from (
-            select user
-                , max(time) as last_time
-            from co_session
-            where time >= unix_timestamp(current_date() - 1) -- only look back 1 day
-            group by user
-        ) as last_times
-        left join co_session as sess on last_times.user = sess.user and last_times.last_time = sess.time
-        left join co_user as usr on last_times.user = usr.rowid
-        where sess.action = 1
-    """
-    return pd.read_sql(online_users, ENG)
+class Fetcher:
+    CMDS = {
+        "commands": GET_COMMANDS,
+        "observations": GET_OBSERVATIONS,
+        "science_tools": GET_SCIENCE_TOOLS,
+    }
+
+    def __init__(self):
+        self.players = get_data(GET_ONLINE_PLAYERS)
+        self.newer_than = TODAY
+        self.commands = pd.DataFrame()
+        self.observations = pd.DataFrame()
+        self.science_tools = pd.DataFrame()
+
+    def fetch_data(self):
+        for key, query in Fetcher.CMDS.items():
+            df = get_data(query, self.newer_than)
+            # Set 'self.<key>' to the new dataframe
+            setattr(self, key, df)
+
+    def on_wakeup(self):
+        now = datetime.now()
+        self.fetch_data()
+
+        # Send all triggers
+        for trigger_name, username in self.triggers():
+            print(f"Triggered '{trigger_name}' for '{username}'")
+            send_trigger(trigger_name, username)
+
+        # Next iteration should /only/ show new data
+        self.newer_than = now
+
+    def triggers(self) -> list[tuple[str, str]]:
+        """
+        Return any triggers as a list of tuple[trigger name, username]
+        """
+        triggers = []
+
+        # TODO add checks here
+        # trigger = ("test", "Poi")
+        # triggers.append(trigger)
+
+        return triggers
 
 
-# print(get_commands())
-print(get_online_players())
+if __name__ == "__main__":
+    import signal
 
-# send_trigger("Poi", "Test")
+    def handle_sigint(sig, frame):
+        # Prevent Ctrl+C from hanging
+        SOCKET.close()
+        print("Stopping!")
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, handle_sigint)
+
+    fetcher = Fetcher()
+    while True:
+        print(f"Wakeup at {datetime.now()}")
+        fetcher.on_wakeup()
+        sleep(5)  # run checks every 5 seconds
