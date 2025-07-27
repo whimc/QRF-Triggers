750d600a5b1127fdf628cc2e000f0a97ca86e176
Jeff Ginger <ginger@illinois.edu>
2025-07-27 15:14:15 -0500

    Debugging

--- maine25am.bat
+++ maine25am.bat
@@ -16,6 +16,6 @@ echo Installing/updating dependencies...
 pip install -r requirements.txt
 
 REM Run the script
-python src.py --initial-newer-than "2025-07-27 8:00:00" --saveload umaine2025am --wid 134
+python src.py --initial-newer-than "2025-07-27 12:30:00" --saveload umaine2025am --wid 134
 
 pause
