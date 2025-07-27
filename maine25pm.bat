03425429e8a7ecad49e91c9d4c90e2a92035cf17
Jeff Ginger <ginger@illinois.edu>
2025-07-27 14:27:36 -0500

    Removed obsolete 127 worldid from init

--- maine25pm.bat
+++ maine25pm.bat
@@ -16,6 +16,6 @@ echo Installing/updating dependencies...
 pip install -r requirements.txt
 
 REM Run the script
-python src.py --initial-newer-than "2025-07-27 8:00:00" --saveload Umaine2025am --wid 133
+python src.py --initial-newer-than "2025-07-27 12:30:00" --saveload umaine2025pm --wid 133
 
 pause
